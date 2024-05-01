a1f7356c25893a3f5ab1f436267c69bc2a25683c
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2024-05-01 22:37:03 +0300

    check loader

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,22 +1,15 @@
 import { useSelector } from "react-redux"
 
-import ErrorMessage from "../errorMessage/ErrorMessage";
 import Contact from "../contact/Contact";
-import Loader from "../loader/Loader";
 import { selectFilteredContacts } from "../redux/filters/slice";
-import { selectPhonebookIsError, selectPhonebookIsLoading } from "../redux/contacts/selector";
 
 import css from "./Contacts.module.css"
 
 const Contacts = () => {
   const filteredContacts = useSelector(selectFilteredContacts);
-  const isLoading = useSelector(selectPhonebookIsLoading)
-  const isError = useSelector(selectPhonebookIsError)
 
   return (
     <div>
-      {isLoading && <Loader />}
-      {isError && <ErrorMessage />}
       <ul className={css.contactsList}>
         {filteredContacts.map((contact) => {
           return (
@@ -27,7 +20,7 @@ const Contacts = () => {
               id={contact.id}
             />
           )
-          })}
+        })}
       </ul>
     </div>
   )
